71d18f3ec960167704a120359414e8604b4e466a
jsr0000 <josh.regnart@gmail.com>
2025-03-19 17:27:56 +0000

    Refactor transaction parameter type from calldata to memory in ZkMinimalAccount contract

--- src/zksync/ZkMinimalAccount.sol
+++ src/zksync/ZkMinimalAccount.sol
@@ -8,30 +8,30 @@ contract ZkMinimalAccount is IAccount {
     function validateTransaction(
         bytes32 _txHash,
         bytes32 _suggestedSignedHash,
-        Transaction calldata _transaction
+        Transaction memory _transaction
     ) external payable returns (bytes4 magic) {}
 
     function executeTransaction(
         bytes32 _txHash,
         bytes32 _suggestedSignedHash,
-        Transaction calldata _transaction
+        Transaction memory _transaction
     ) external payable {}
 
     // There is no point in providing possible signed hash in the `executeTransactionFromOutside` method,
     // since it typically should not be trusted.
     function executeTransactionFromOutside(
-        Transaction calldata _transaction
+        Transaction memory _transaction
     ) external payable {}
 
     function payForTransaction(
         bytes32 _txHash,
         bytes32 _suggestedSignedHash,
-        Transaction calldata _transaction
+        Transaction memory _transaction
     ) external payable {}
 
     function prepareForPaymaster(
         bytes32 _txHash,
         bytes32 _possibleSignedHash,
-        Transaction calldata _transaction
+        Transaction memory _transaction
     ) external payable {}
 }
